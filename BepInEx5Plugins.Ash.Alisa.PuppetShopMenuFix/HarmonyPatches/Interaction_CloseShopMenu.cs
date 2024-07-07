1541e23d096d4150b82d7a5819a600833f06030d
ash <TakoNekko@users.noreply.github.com>
2024-07-07 19:35:36 +0700

    Initial release

---
+++ BepInEx5Plugins.Ash.Alisa.PuppetShopMenuFix/HarmonyPatches/Interaction_CloseShopMenu.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace BepInEx5Plugins.Ash.Alisa.PuppetShopMenuFix.HarmonyPatches
+{
+	[HarmonyPatch(typeof(Interaction), "CloseShopMenu")]
+	public class Interaction_CloseShopMenu
+	{
+		// Disable button selection auto fix.
+		public static void Prefix(Interaction __instance
+			//, ref EquipmentMenu ___equipmentMenuScript
+			)
+		{
+			//if (!___equipmentMenuScript.buttonLock)
+			{
+				PuppetShopMenu_Update.checkPending = false;
+			}
+		}
+	}
+}
---
+++ BepInEx5Plugins.Ash.Alisa.PuppetShopMenuFix/HarmonyPatches/ShopCancelButton_OnSelect.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine.EventSystems;
+
+namespace BepInEx5Plugins.Ash.Alisa.PuppetShopMenuFix.HarmonyPatches
+{
+	[HarmonyPatch(typeof(ShopCancelButton), "OnSelect", new Type[] { typeof(BaseEventData) })]
+	public class ShopCancelButton_OnSelect
+	{
+		// Disable button selection auto fix.
+		public static void Prefix(ShopCancelButton __instance, BaseEventData eventData)
+		{
+			PuppetShopMenu_Update.checkPending = false;
+		}
+	}
+}
