492e1e71fd5034d03eaf9c1a4cbccd5cdc3ee4ad
ash <TakoNekko@users.noreply.github.com>
2024-07-07 17:58:12 +0700

    Initial release

---
+++ BepInEx5Plugins.Ash.Alisa.TextScrolling/HarmonyPatches/TextBoxManager_EnableTextBox.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using Object = UnityEngine.Object;
+
+namespace BepInEx5Plugins.Ash.Alisa.TextScrolling.HarmonyPatches
+{
+	[HarmonyPatch(typeof(TextBoxManager), "EnableTextBox")]
+	public class TextBoxManager_EnableTextBox
+	{
+		// Replace call to TextScroll.
+		public static bool Prefix(TextBoxManager __instance
+			, ref bool ___cantSkip
+			)
+		{
+			___cantSkip = true;
+			__instance.isActive = true;
+
+			var interaction = Object.FindObjectOfType<Interaction>();
+
+			if (interaction)
+			{
+				interaction.interactionIsActive = true;
+			}
+
+			if (__instance.isScrollingText)
+			{
+				__instance.StartCoroutine(TextBoxManager_Update.TextScroll(__instance, __instance.textLines[__instance.currentLine]));
+			}
+
+			return false;
+		}
+	}
+}
