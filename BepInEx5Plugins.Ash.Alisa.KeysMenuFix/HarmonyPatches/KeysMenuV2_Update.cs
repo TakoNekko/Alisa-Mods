290cedc946025c3bf66ff995ef66b9af069381f4
ash <TakoNekko@users.noreply.github.com>
2024-07-08 11:44:36 +0700

    Initial release

---
+++ BepInEx5Plugins.Ash.Alisa.KeysMenuFix/HarmonyPatches/KeysMenuV2_Update.cs
@@ -0,0 +1,76 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace BepInEx5Plugins.Ash.Alisa.KeysMenuFix.HarmonyPatches
+{
+	[HarmonyPatch(typeof(KeysMenuV2), "Update")]
+	public class KeysMenuV2_Update
+	{
+		private static MethodInfo KeysMenuV2_SelectedItem;
+		private static MethodInfo KeysMenuV2_Move;
+		private static MethodInfo KeysMenuV2_Zoom;
+		private static MethodInfo KeysMenuV2_CloseKeysMenu;
+		private static MethodInfo KeysMenuV2_ItemFunction;
+		private static MethodInfo KeysMenuV2_KeyRingUpdate;
+		
+		public static bool Prepare(MethodBase original)
+		{
+			if (original is null)
+			{
+				try
+				{
+					KeysMenuV2_SelectedItem = typeof(KeysMenuV2).GetMethod("SelectedItem", BindingFlags.NonPublic | BindingFlags.Instance);
+					KeysMenuV2_Move = typeof(KeysMenuV2).GetMethod("Move", BindingFlags.NonPublic | BindingFlags.Instance);
+					KeysMenuV2_Zoom = typeof(KeysMenuV2).GetMethod("Zoom", BindingFlags.NonPublic | BindingFlags.Instance);
+					KeysMenuV2_CloseKeysMenu = typeof(KeysMenuV2).GetMethod("CloseKeysMenu", BindingFlags.NonPublic | BindingFlags.Instance);
+					KeysMenuV2_ItemFunction = typeof(KeysMenuV2).GetMethod("ItemFunction", BindingFlags.NonPublic | BindingFlags.Instance);
+					KeysMenuV2_KeyRingUpdate = typeof(KeysMenuV2).GetMethod("KeyRingUpdate", BindingFlags.NonPublic | BindingFlags.Instance);
+				}
+				catch (Exception exception)
+				{
+					Console.WriteLine(exception);
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		// Call the Zoom method even if currentSelectedItem is null.
+		public static void Postfix(KeysMenuV2 __instance)
+		{
+			if (!__instance.locked)
+			{
+				__instance.RayCheckItem();
+				KeysMenuV2_SelectedItem.Invoke(__instance, null);
+				KeysMenuV2_Move.Invoke(__instance, null);
+				KeysMenuV2_Zoom.Invoke(__instance, null);
+
+				if (__instance.currentSelectedItem)
+				{
+					KeysMenuV2_CloseKeysMenu.Invoke(__instance, null);
+					KeysMenuV2_ItemFunction.Invoke(__instance, null);
+				}
+
+				if (__instance.keyRing_Obj.activeSelf)
+				{
+					KeysMenuV2_KeyRingUpdate.Invoke(__instance, null);
+				}
+			}
+
+			if (__instance.locked
+				&& __instance.currentSelectedItem)
+			{
+				__instance.currentSelectedItem.transform.rotation = Quaternion.identity;
+			}
+
+			if (!__instance.itemList.itemsTotal[0].activeSelf
+				&& __instance.titleNone.activeSelf)
+			{
+				__instance.titleNone.SetActive(false);
+			}
+		}
+	}
+}
---
+++ BepInEx5Plugins.Ash.Alisa.KeysMenuFix/HarmonyPatches/KeysMenuV2_Zoom.cs
@@ -0,0 +1,76 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BepInEx5Plugins.Ash.Alisa.KeysMenuFix.HarmonyPatches
+{
+	[HarmonyPatch(typeof(KeysMenuV2), "Zoom")]
+	public class KeysMenuV2_Zoom
+	{
+		// Check if currentSelectedItem is null before referencing it.
+		public static bool Prefix(KeysMenuV2 __instance
+			, ref Settings ___settings
+			, ref bool ___moving
+			, ref int ___zoom
+			, ref Vector3 ___zoomedIn
+			, ref float ___moveSpeed
+			, ref AudioSource ___audioSource
+			)
+		{
+			if (__instance.currentSelectedItem
+				&& __instance.currentSelectedItem.name == "KeyRing"
+				&& Input.GetKeyDown(___settings.keys["Action"])
+				&& ___zoom == 0
+				&& !___moving)
+			{
+				___zoom = 1;
+				___moving = true;
+				__instance.currentSelectedItem.transform.rotation = Quaternion.identity;
+				___audioSource.PlayOneShot(__instance.keyRingZoomInSound, 1f);
+			}
+			else if (__instance.currentSelectedItem
+				&& __instance.currentSelectedItem.name == "KeyRing"
+				&& Input.GetKeyDown(___settings.keys["Cancel"])
+				&& ___zoom == 2)
+			{
+				___zoom = 3;
+				___audioSource.PlayOneShot(__instance.keyRingZoomOutSound, 1f);
+			}
+
+			if (___zoom == 1)
+			{
+				__instance.rayPoint.localPosition = Vector3.MoveTowards(__instance.rayPoint.localPosition, ___zoomedIn, ___moveSpeed * Time.deltaTime);
+
+				if (__instance.rayPoint.localPosition == ___zoomedIn)
+				{
+					___zoom = 2;
+				}
+			}
+			else if (___zoom == 3)
+			{
+				__instance.rayPoint.localPosition = Vector3.MoveTowards(__instance.rayPoint.localPosition, Vector3.zero, ___moveSpeed * Time.deltaTime);
+
+				if (__instance.rayPoint.localPosition == Vector3.zero)
+				{
+					___zoom = 0;
+					___moving = false;
+				}
+			}
+
+			if (__instance.currentSelectedItem
+				&& __instance.currentSelectedItem.name == "KeyRing"
+				&& ___zoom == 0
+				&& !__instance.keyRingTitle.activeSelf)
+			{
+				__instance.keyRingTitle.SetActive(true);
+			}
+			else if ((__instance.currentSelectedItem
+				&& __instance.currentSelectedItem.name != "KeyRing" || ___zoom != 0)
+				&& __instance.keyRingTitle.activeSelf)
+			{
+				__instance.keyRingTitle.SetActive(false);
+			}
+
+			return false;
+		}
+	}
+}
