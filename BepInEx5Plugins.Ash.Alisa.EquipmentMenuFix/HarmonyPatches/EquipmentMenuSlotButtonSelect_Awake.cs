d9887deb47d22177f8c37b12082b7393988562be
ash <TakoNekko@users.noreply.github.com>
2024-07-13 10:29:07 +0700

    Initial release

---
+++ BepInEx5Plugins.Ash.Alisa.EquipmentMenuFix/HarmonyPatches/EquipmentMenuSlotButtonSelect_Awake.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using UnityEngine.UI;
+
+namespace BepInEx5Plugins.Ash.Alisa.EquipmentMenuFix.HarmonyPatches
+{
+	[HarmonyPatch(typeof(EquipmentMenuSlotButtonSelect), "Awake")]
+	public class EquipmentMenuSlotButtonSelect_Awake
+	{
+		// Set the disabled state to be the same as the normal state.
+		public static void Postfix(EquipmentMenuSlotButtonSelect __instance)
+		{
+			var button = __instance.GetComponent<Button>();
+
+			if (button)
+			{
+				if (button.transition == Selectable.Transition.SpriteSwap)
+				{
+					var spriteState = button.spriteState;
+
+					spriteState.disabledSprite = button.image.sprite;
+
+					button.spriteState = spriteState;
+				}
+				else if (button.transition == Selectable.Transition.ColorTint)
+				{
+					var colorBlock = button.colors;
+
+					colorBlock.disabledColor = colorBlock.normalColor;
+
+					button.colors = colorBlock;
+				}
+			}
+		}
+	}
+}
---
+++ BepInEx5Plugins.Ash.Alisa.EquipmentMenuFix/HarmonyPatches/EquipmentMenuSlotButtonSelect_OnSelect.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace BepInEx5Plugins.Ash.Alisa.EquipmentMenuFix.HarmonyPatches
+{
+	[HarmonyPatch(typeof(EquipmentMenuSlotButtonSelect), "OnSelect", new Type[] { typeof(BaseEventData) })]
+	public class EquipmentMenuSlotButtonSelect_OnSelect
+	{
+		// Prevent processing when button is not interactable.
+		public static bool Prefix(EquipmentMenuSlotButtonSelect __instance, BaseEventData eventData)
+		{
+			try
+			{
+				var button = __instance.GetComponent<Button>();
+
+				if (!button)
+				{
+					return true;
+				}
+
+				if (button && !button.interactable)
+				{
+					return false;
+				}
+
+				if (button)
+				{
+					var canvasGroup = button.transform.parent?.parent?.GetComponent<CanvasGroup>();
+
+					if (canvasGroup && !canvasGroup.interactable)
+					{
+						return false;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+
+			return true;
+		}
+	}
+}
